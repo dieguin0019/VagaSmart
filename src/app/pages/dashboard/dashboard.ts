015d6af72ef439228dea52c26b0acf247d1a5c35
Rafichera <raficho.exe@gmail.com>
2025-11-24 23:14:45 -0300

    organização dos modals do dashboard, outros ajustes

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,8 +1,9 @@
+import { ModalEncontrarVaga } from './../../components/modal-encontrar-vaga/modal-encontrar-vaga';
 import { Component, inject } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
 import { Dialog } from '@angular/cdk/dialog';
-import { ModalComponent } from '../../components/modal/modal';
 import { Footer } from "../../components/footer/footer";
+import { ModalAdicionarVeiculo } from '../../components/modal-adicionar-veiculo/modal-adicionar-veiculo';
 
 
 @Component({
@@ -16,29 +17,17 @@ export class Dashboard {
 
   dialog = inject(Dialog);
 
-  openModal() {
-    this.dialog.open(ModalComponent, {
+  openModalVaga() {
+    this.dialog.open(ModalEncontrarVaga, {
       hasBackdrop: true,
       disableClose: true,
-      data: {
-        titulo: 'Adicionar Veiculo',
-        input1: 'Placa do Veiculo',
-        input2: 'Modelo do Veiculo',
-        condicao: 'adicionar'
-      }
     });
   }
 
-  encontrarVaga() {
-    this.dialog.open(ModalComponent, {
+  openModalVeiculo() {
+    this.dialog.open(ModalAdicionarVeiculo, {
       hasBackdrop: true,
       disableClose: true,
-      data: {
-        titulo: 'Encontrar Vaga',
-        input1: '',
-        input2: 'valor do segundo input',
-        condicao: 'encontrar'
-      }
     });
   }
 
